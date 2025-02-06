b71883fcf1105f077a8edf48677104452de4f8db
Tony Santana <tonysantana1492@gmail.com>
2025-02-05 21:43:00 -0500

    chore(): update Prettier configuration and clean up project files

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   minify: true,
   target: 'es2018',
-  external: ['react', './xhr-sync-worker.js'],
+  external: ['react'],
   sourcemap: true,
   dts: true,
   format: ['esm', 'cjs'],
